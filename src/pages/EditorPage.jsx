66abbd76f3737ace738d10649c654f912383910a
Alexander Solovkin <124513715+L4sTH0qE@users.noreply.github.com>
2026-05-29 00:10:04 +0300

    Update EditorPage.jsx

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -429,7 +429,7 @@ const EditorPage = () => {
 
             <Layout style={{ flexDirection: 'row' }}>
                 <Sider width='10vw' theme="light" style={{ borderRight: '1px solid #eee' }}><Sidebar /></Sider>
-                <Content style={{ overflowY: 'auto', justifyContent: 'center', padding: '20px', background: 'var(--hse-gray-accent)' }}><Canvas name={title} /></Content>
+                <Content style={{ overflowY: 'auto', marginLeft: '8px', padding: '12px', background: 'var(--hse-gray-accent)' }}><Canvas name={title} /></Content>
                 <Sider width='30vw' theme="light" style={{ borderLeft: '1px solid #eee' }}><PropertiesPanel /></Sider>
             </Layout>
 
